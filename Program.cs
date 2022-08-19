4d658b17a5339f9f55762f3103db1c75bf62d44a
Hannity Vayne <hvayne@pm.me>
2022-08-20 01:00:20 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ApartmentCrawler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Browser browser = new();
+            var resp = browser.UrlGetToHttp("https://www.myhome.ge/en/s/?AdTypeID=1&PrTypeID=1&Page=27&SortID=1&Ajax=1");
+            var respstr = resp.Result.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
